c8b533dd5b4c614d6542e46d1e052c8e05c57f96
Jerica Yuan <jxyuan@uci.edu>
2026-02-08 15:56:35 -0800

    restructure backend into src + routes and controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 5 notes");
-});
+app.use("/api/notes", notesRoutes);
 
 
 app.listen(5001, () => {
