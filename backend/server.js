95c15e784897c3a31717368fb2cceea14b258e65
Jerica Yuan <jxyuan@uci.edu>
2026-02-08 15:08:25 -0800

    initial backend setup with Express server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 5 notes");
+});
+
+
+app.listen(5001, () => {
+    console.log("Server started on port: 5001");
+})
