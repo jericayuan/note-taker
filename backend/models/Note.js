d215247e08ef020dbee0e0079f72139f6b7f9569
Jerica Yuan <jxyuan@uci.edu>
2026-02-08 19:09:16 -0800

    note schema + model

---
+++ backend/models/Note.js
@@ -0,0 +1,22 @@
+import mongoose, { mongo } from "mongoose";
+
+// 1 create schema
+// 2 create model based off schema
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+    }, 
+    {timestamps:true} // createdAt/updatedAt fields
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note
