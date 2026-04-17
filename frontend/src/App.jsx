50f430ecd79ba9e396f9e475b721c3a3fc30e5e5
jericayuan <jxyuan@uci.edu>
2026-04-16 22:27:52 -0700

    scaffold React frontend w/routing and page stubs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
+import { Route, Routes } from 'react-router';
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <button className='btn btn-outline'>Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
